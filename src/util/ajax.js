f87b2c3f1fdd3ff502af00df8e4d75cc792586d8
shengxinyuan <461415703@qq.com>
2021-10-23 12:51:50 +0800

    fix 调试token

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -8,6 +8,8 @@ const getRequest = method => {
   return ({ url, data, options = {}, isForm = true }) => {
     let tokenHeader = {}
     if (window.top.uni && window.top.uni.getStorageSync) {
+      console.log('in uni');
+      console.log('window.top.uni', );
       console.log(window.top.uni.getStorageSync('token'));
       tokenHeader.token = window.top.uni.getStorageSync('token')
     }
